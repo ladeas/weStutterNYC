93aa7ba7a0fa97b306d31650f7383df29ecab009
suavros <stavros81@gmail.com>
2013-08-19 16:00:32 -0400

    first commit

---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,11 @@
+class InfoController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeStutterNYC::Application.config.secret_key_base = '020f2bdeebff726c79de57fef8ff54ce3d3f273bbd5e93add785261eb474c9e644a8442b5f30e9ae6308996bb1fe5842fab2c96a224737d53c2c4e131b5d9ebc'
