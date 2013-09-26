c81a9616a153e4d292091f186d459393cb017104
suavros <stavros81@gmail.com>
2013-09-26 17:51:31 -0400

    trying to fix heroku css problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ WeStutterNYC::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w(base.css flexslider.css media.queries.css tipsy.css)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
