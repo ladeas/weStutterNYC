31f4e420a717cf1722c240ddbf2e1456db01c020
suavros <stavros.ladeas@gmail.com>
2014-05-10 23:20:05 -0400

    changing settings still

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,20 +6,12 @@ class ApplicationController < ActionController::Base
   private
 
   def mobile_device?
-    if session[:mobile_param]
-      session[:mobile_param] == "1"
-    else
-      request.user_agent =~ /Mobile|webOS/
-    end
+  request.user_agent =~ /Mobile|webOS/
   end
   helper_method :mobile_device?
+end
 
-  def prepare_for_mobile
-    session[:mobile_param] = params[:mobile] if params[:mobile]
-    request.format = :mobile if mobile_device?
-  end
 
-  
   protect_from_forgery with: :exception
 
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
